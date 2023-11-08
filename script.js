400f464523d8df3e4982bf3ef343117318303399
angelaszalkai <angela.szalkai@gmail.com>
2023-11-08 13:15:22 +0200

    copied from github

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
-// ------- HIANYOS, LESZEDNI GITHUBROL!!! -------
+const rootElement = document.querySelector("#root")
+
+const fetchUrl = (url) => fetch(url).then(res => res.json())
+
+const skeletonComponent = () => `
+  <div class="characters"></div>
+  <div class="buttons"></div>
+`
+
+const characterComponent = (characterData) => ` 
+  <div class="char">
+    <img src=${characterData.image}>
+    <h2>${characterData.name}</h2>
+    <h3>appears in: ${characterData.episode.length} episodes</h3>
+  </div>
+`
+
+const buttonComponent = (id, text) => `<button id=${id}>${text}</button>`
+
+const buttonEventComponent = (id, url) => {
+  const buttonElement = document.querySelector(`#${id}`)
+  buttonElement.addEventListener("click", () => {
+    console.log(`fetch: ${url}`)
+    rootElement.innerHTML = "LOADING..."
+    fetchUrl(url).then(data => makeDomFromData(data, rootElement))
+  })
+}
+
+const makeDomFromData = (data, rootElement) => {
+  rootElement.innerHTML = skeletonComponent()
+
+  const charactersElement = document.querySelector(".characters")
+  const buttonsElement = document.querySelector(".buttons") 
+
+  const info = data.info
+  const characters = data.results
+
+  characters.forEach(character => charactersElement.insertAdjacentHTML("beforeend", characterComponent(character)))
+
+  if (info.prev) {
+    buttonsElement.insertAdjacentHTML("beforeend", buttonComponent("prev", "previous"))
+    buttonEventComponent("prev", info.prev)
+  }
+
+  if (info.next) {
+    buttonsElement.insertAdjacentHTML("beforeend", buttonComponent("next", "next"))
+    buttonEventComponent("next", info.next)
+  }
+}
+
+const init = () => {
+  rootElement.innerHTML = "LOADING..."
+  fetchUrl("https://rickandmortyapi.com/api/character").then(data => makeDomFromData(data, rootElement))
+}
+
+init()
 
 
+
+
+
+
+/*
+//HIANYOS, EZ AZ, AMIT EN IRTAM A WS-ON
 
 const rootElement = document.querySelector(#root)
 
 const fetchUrl = (url) => fetch(url).then(res => res.json())
-
+*/
 /*
 //FETCH THEN-NEL (ASYNC AWAT HELYETT):
 fetch("https://rickandmortyapi.com/api/character")
@@ -14,7 +75,7 @@ fetch("https://rickandmortyapi.com/api/character")
         console.log(data)
     })
 */
-
+/*
 //returns an HTML string with the values of character object
 const characterComponent = (characterData) => `
     <div class="char">
@@ -23,6 +84,7 @@ const characterComponent = (characterData) => `
         <h3>appears in: ${character.episode.length} episodes</h3>
     </div>
 `
+
 
 //button component
 const buttonComponent = (id, text) => `<button id=${id}>${text}</button>`
@@ -49,9 +111,7 @@ const makeDomFromData = (data, rootElement) => {
     }
 
 
-
 }
-
 
 
 const init = () => {
@@ -60,3 +120,4 @@ const init = () => {
 }
 
 init()
+*/
